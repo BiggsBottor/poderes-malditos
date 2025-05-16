163966544b2a910d3bc5141a8c835ec5755addba
BiggsBottor <53606900+BiggsBottor@users.noreply.github.com>
2025-05-16 16:59:51 +0200

    remove a console log

--- scripts/script.js
+++ scripts/script.js
@@ -109,7 +109,6 @@ const Traits = {
   miedo: 2
 };
 let powerIndex = 0;
-console.log('%cTraits on load', 'color:orange', {Traits});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
